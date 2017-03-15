b85e3aab5f46c7f2a5acfe07c64764ed5e3694a2
Keo Chau <kc@Keos-MacBook-Pro.local>
2017-03-15 02:08:30 -0400

    First

---
+++ test.py
@@ -0,0 +1,12 @@
+import Tkinter
+parent_widget = T
+kinter.Tk()
+label_widget = Tkinter.Label(parent_widget, text="Hello, welcome to this Short Term Memory Test")
+label_widget.pack()
+Tkinter.mainloop()
+
+parent_widget = Tkinter.Tk()
+entry_widget = Tkinter.Entry(parent_widget)
+entry_widget.insert(0, "Please type in your name")
+entry_widget.pack()
+Tkinter.mainloop()
