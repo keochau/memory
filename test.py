e3b3dfeed2fc9a25b77b15c5606d57718fb9bacf
Keo Chau <kc@Keos-MacBook-Pro.local>
2017-03-21 15:32:17 -0400

    Redo

--- test.py
+++ test.py
@@ -1,33 +1,60 @@
-import Tkinter
-parent_widget = Tkinter.Tk()
-label_widget = Tkinter.Label(parent_widget, text="Hello, welcome to this Short Term Memory Test")
-label_widget.pack()
-Tkinter.mainloop()
+from Tkinter import *
+root = Tk()
 
-parent_widget = Tkinter.Tk()
-v = Tkinter.IntVar()
-v.set(1)
-radiobutton_widget1 = Tkinter.Radiobutton(parent_widget,
-                                   text="New User",
-                                   variable=v, value=1,
-                                   indicatoron=False)
-radiobutton_widget2 = Tkinter.Radiobutton(parent_widget,
-                                   text="Returning User",
-                                   variable=v, value=2,
-                                   indicatoron=False)
-radiobutton_widget1.pack()
-radiobutton_widget2.pack()
-Tkinter.mainloop()
+text1 = Text(root, height=20, width=30)
+photo=PhotoImage(file='/Users/kc/Desktop/memory/i2.jpg')
+text1.insert(END,'\n')
+text1.image_create(END, image=photo)
 
-parent_widget = Tkinter.Tk()
-entry_widget = Tkinter.Entry(parent_widget)
-entry_widget.insert(0, "Please type in your name")
-entry_widget.pack()
-Tkinter.mainloop()
+text1.pack(side=LEFT)
 
-parent_widget = Tkinter.Tk()
-checkbutton_widget = Tkinter.Checkbutton(parent_widget,
-                                    text="Justin Wang")
-checkbutton_widget.select()
-checkbutton_widget.pack()
-Tkinter.mainloop()
+text2 = Text(root, height=20, width=50)
+scroll = Scrollbar(root, command=text2.yview)
+text2.configure(yscrollcommand=scroll.set)
+text2.tag_configure('bold_italics', font=('Arial', 12, 'bold', 'italic'))
+text2.tag_configure('big', font=('Verdana', 20, 'bold'))
+text2.tag_configure('color', foreground='#476042', 
+						font=('Tempus Sans ITC', 12, 'bold'))
+text2.tag_bind('follow', '<1>', lambda e, t=text2: t.insert(END, "Not now, maybe later!"))
+text2.insert(END,'\nWelcome\n', 'big')
+quote = """
+This short term memory test is very simple. Try your best to memorize the phrase on the screen and type what you remember when asked
+"""
+text2.insert(END, quote, 'color')
+text2.insert(END, 'follow-up\n', 'follow')
+text2.pack(side=LEFT)
+scroll.pack(side=RIGHT, fill=Y)
+
+root.mainloop()
+
+from Tkinter import *
+fields = 'First Name', 'Last Name', 'Age'
+
+def fetch(entries):
+   for entry in entries:
+      field = entry[0]
+      text  = entry[1].get()
+      print('%s: "%s"' % (field, text)) 
+
+def makeform(root, fields):
+   entries = []
+   for field in fields:
+      row = Frame(root)
+      lab = Label(row, width=15, text=field, anchor='w')
+      ent = Entry(row)
+      row.pack(side=TOP, fill=X, padx=5, pady=5)
+      lab.pack(side=LEFT)
+      ent.pack(side=RIGHT, expand=YES, fill=X)
+      entries.append((field, ent))
+   return entries
+
+if __name__ == '__main__':
+   root = Tk()
+   ents = makeform(root, fields)
+   root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
+   b1 = Button(root, text='Continue',
+          command=(lambda e=ents: fetch(e)))
+   b1.pack(side=LEFT, padx=5, pady=5)
+   b2 = Button(root, text='Quit', command=root.quit)
+   b2.pack(side=LEFT, padx=5, pady=5)
+   root.mainloop()
